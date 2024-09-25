5299b3912b67207e22cfc80e2ef81e1ff84216c5
Bruno Silva <bruno.ogs@gmail.com>
2024-09-24 23:02:54 -0400

    first commit

---
+++ logging_exemplo_erros.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.debug("Um aviso para o sesenvolvedor no futuro")
+logger.info("Informacao do processo")
+logger.warning("Um aviso que algo vai parar de funcionar no futuro")
+logger.error("Aconteceu uma falha")
+logger.critical("Aconteceu uma falha que aborta a aplicacao")
